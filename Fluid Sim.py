fa87811446345987fcd714d3f9667f3019428930
David Burke <burkedm94@gmail.com>
2023-12-23 11:44:31 -0500

    add controls

--- Fluid Sim.py
+++ Fluid Sim.py
@@ -2,22 +2,25 @@ import pygame, random, math, time, numpy as np, pygame.surfarray as surfarray
 
 pygame.init()
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
-SIZE = 5
-PARTICLE_DISTRIBUTION = 5
+SCREEN_WIDTH = 500
+SCREEN_HEIGHT = 500
+SQUARE_SIZE = 5
+PARTICLE_DISTRIBUTION = 3
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 particle_surface = pygame.Surface((SCREEN_WIDTH, SCREEN_HEIGHT))
 prev_time = time.time()
-ROWS = int(SCREEN_WIDTH / SIZE)
-COLS = int(SCREEN_HEIGHT / SIZE)
-prev_pos = (0,0)
+prev_mouse_pos = (0, 0)
+mouse_angle = 0
+ROWS = int(SCREEN_WIDTH / SQUARE_SIZE)
+COLS = int(SCREEN_HEIGHT / SQUARE_SIZE)
 test_value = 0
 num_particles = int(SCREEN_WIDTH * SCREEN_HEIGHT / (PARTICLE_DISTRIBUTION**2))
 font = pygame.font.Font(None, 20)
 
 
 show_fps = True
+refresh_each_frame = True
+pump_speed = 0
 
 square_dtype = np.dtype([
     ('place', int, (2,)),
@@ -39,38 +42,49 @@ for i in range(num_particles):
     y = (i // (SCREEN_WIDTH // PARTICLE_DISTRIBUTION)) * PARTICLE_DISTRIBUTION
     r = 255 * x / SCREEN_WIDTH
     g = 255 * y / SCREEN_HEIGHT
-    b = 255
+    b = 255 - (r + g) // 2
     particles[i]['position'] = [x, y]
     particles[i]['color'] = r + 255 * g + 255 * 255 * b
+
+#Reset colors
+def reset_colors():
+    for i in range(num_particles):
+        [x, y] = particles[i]['position']
+        r = 255 * x / SCREEN_WIDTH
+        g = 255 * y / SCREEN_HEIGHT
+        b = 255 - (r + g) // 2
+        particles[i]['color'] = r + 255 * g + 255 * 255 * b
 
 #Initialize squares
 squares = np.zeros((ROWS,COLS), dtype = square_dtype)
 for i in range(ROWS):
     for j in range(COLS):
-        pos = (i * SIZE, j * SIZE)
+        pos = (i * SQUARE_SIZE, j * SQUARE_SIZE)
         place = (i, j)
         flow = (random.random() * 2 - 1, random.random() * 2 - 1)
-        squares[i, j] = (place, pos, SIZE, flow, 0)
+        squares[i, j] = (place, pos, SQUARE_SIZE, flow, 0)
 
 run = True
 q = 0
 
-def draw_flowpaths():
+def add_pumps():
+    if pump_speed == 0:
+        return
     for i in range (ROWS):
-        for j in range (int(COLS * 0), int(COLS * .03)):
-            squares[i, j]['flow'] = (-1, squares[i, j]['flow'][1])
+        for j in range (int(COLS * 0), int(COLS * .05)):
+            squares[i, j]['flow'] = (-pump_speed, squares[i, j]['flow'][1])
 
     for i in range (ROWS):
-        for j in range (int(COLS * 0.5), int(COLS * .53)):
-            squares[i, j]['flow'] = (1, squares[i, j]['flow'][1])
+        for j in range (int(COLS * 0.5), int(COLS * .55)):
+            squares[i, j]['flow'] = (pump_speed, squares[i, j]['flow'][1])
 
-    for i in range (int(ROWS * 0), int(ROWS * .03)):
+    for i in range (int(ROWS * 0), int(ROWS * .05)):
         for j in range (COLS):
-            squares[i, j]['flow'] = (squares[i, j]['flow'][0], -1)
+            squares[i, j]['flow'] = (squares[i, j]['flow'][0], -pump_speed)
 
-    for i in range (int(ROWS * .5), int(ROWS * .53)):
+    for i in range (int(ROWS * .5), int(ROWS * .55)):
         for j in range (COLS):
-            squares[i, j]['flow'] = (squares[i, j]['flow'][0], 1)
+            squares[i, j]['flow'] = (squares[i, j]['flow'][0], pump_speed)
 
 def update_squares():
     adjacent_positions = [(0,1),(1,0),(0,-1),(-1,0)]
@@ -93,8 +107,8 @@ def update_squares():
         density_gradient_y += dj * density_diff
 
     #average the flow at a 6/4 ratio of current and surrounding ave
-    next_flow_x = (flow_x_sum + squares['flow'][:,:,0] * 6 + density_gradient_x * .1) * 0.1
-    next_flow_y = (flow_y_sum + squares['flow'][:,:,1] * 6 + density_gradient_y * .1) * 0.1
+    next_flow_x = (flow_x_sum + squares['flow'][:,:,0] * 6 + density_gradient_x * delta_time) * 0.1
+    next_flow_y = (flow_y_sum + squares['flow'][:,:,1] * 6 + density_gradient_y * delta_time) * 0.1
 
     #update flow
     squares['flow'] = np.stack((next_flow_x, next_flow_y), axis = -1)
@@ -107,11 +121,11 @@ def update_particles():
     #Wrap boundaries
     positions = particles['position'] 
     densities = squares['density']
-    positions %= [SIZE * ROWS, SIZE * COLS]
+    positions %= [SQUARE_SIZE * ROWS, SQUARE_SIZE * COLS]
 
     #Find grid indices for flow lookup
-    grid_x = np.clip(np.floor(positions[:, 0] / SIZE).astype(int), 0, ROWS - 1)
-    grid_y = np.clip(np.floor(positions[:, 1] / SIZE).astype(int), 0, COLS - 1)
+    grid_x = np.clip(np.floor(positions[:, 0] / SQUARE_SIZE).astype(int), 0, ROWS - 1)
+    grid_y = np.clip(np.floor(positions[:, 1] / SQUARE_SIZE).astype(int), 0, COLS - 1)
     flows = squares[grid_x, grid_y]['flow']
 
     #Update positions
@@ -122,7 +136,8 @@ def update_particles():
 
     #get pixels as np array (locks particle_surface)
     pixels = surfarray.pixels2d(particle_surface)
-    pixels.fill(0)
+    if (refresh_each_frame):
+        pixels.fill(0)
 
     #color pixels at particle positions
     screen_positions = np.clip(positions.astype(int), [0, 0], [SCREEN_WIDTH - 1, SCREEN_HEIGHT - 1])
@@ -136,8 +151,10 @@ def update_particles():
 while run:
     delta_time = time.time() - prev_time
     prev_time = time.time()
-
-    #screen.fill((0,0,0))
+    mouse_pos = pygame.mouse.get_pos()
+    if mouse_pos != prev_mouse_pos:
+        mouse_angle = math.atan2((mouse_pos[1] - prev_mouse_pos[1]), (mouse_pos[0] - prev_mouse_pos[0]))
+        prev_mouse_pos = pygame.mouse.get_pos()
 
     if (show_fps):
         if delta_time != 0:
@@ -148,8 +165,34 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+        if event.type == pygame.KEYDOWN:
+            match event.key:
+                case pygame.K_r:
+                    refresh_each_frame = not refresh_each_frame
+                case pygame.K_t:
+                    reset_colors()
+                case pygame.K_0:
+                    pump_speed = 0
+                case pygame.K_1:
+                    pump_speed = 1
+                case pygame.K_2:
+                    pump_speed = 2
+                case pygame.K_3:
+                    pump_speed = 3
+                case pygame.K_4:
+                    pump_speed = 4
+                case pygame.K_5:
+                    pump_speed = 5
+    if pygame.mouse.get_pressed()[0]:
+        mouse_index = (mouse_pos[0] // SQUARE_SIZE, mouse_pos[1] // SQUARE_SIZE)
+        flow_dir = (math.cos(mouse_angle), math.sin(mouse_angle))
+        for i in range(-1,1):
+            for j in range (-1,1):
+                xi = (mouse_index[0] + i) % COLS
+                yj = (mouse_index[1] + j) % ROWS
+                squares[xi,yj]['flow'] = [5 * flow_dir[0], 5 * flow_dir[1]]
 
-    draw_flowpaths()
+    add_pumps()
 
     update_squares()
 
